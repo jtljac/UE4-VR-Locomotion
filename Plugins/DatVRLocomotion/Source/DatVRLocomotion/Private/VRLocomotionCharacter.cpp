ae0959b5c3bb8eaad94088037ba62454b95914b2
Jacob <jtljac2@gmail.com>
2020-11-20 02:46:53 +0000

    Created VR Locomotion character

---
+++ Plugins/DatVRLocomotion/Source/DatVRLocomotion/Private/VRLocomotionCharacter.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRLocomotionCharacter.h"
+#include "IXRTrackingSystem.h"
+#include "Engine.h"
+
+// Sets default values
+AVRLocomotionCharacter::AVRLocomotionCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	setupComponents();
+
+	// Setup Variables
+	bUseControllerRotationYaw = false;
+}
+
+void AVRLocomotionCharacter::setupComponents() {
+	HMDOrigin = CreateDefaultSubobject<USceneComponent>(TEXT("HMD Origin"));
+	HMDOrigin->SetupAttachment(RootComponent);
+
+	CameraComponent = CreateDefaultSubobject<UVRCameraComponent>(TEXT("VR Camera"));
+	CameraComponent->SetupAttachment(HMDOrigin);
+	CameraComponent->bUseLateUpdate = false;
+	CameraComponent->bLockToHmd = 1;
+
+	MCOrigin = CreateDefaultSubobject<USceneComponent>(TEXT("MC Origin"));
+	MCOrigin->SetupAttachment(RootComponent);
+
+	LeftMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("Left MC"));
+	LeftMotionController->SetupAttachment(MCOrigin);
+	LeftMotionController->bDisplayDeviceModel = true;
+	LeftMotionController->MotionSource = FName("Left");
+
+	RightMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("Right MC"));
+	RightMotionController->SetupAttachment(MCOrigin);
+	RightMotionController->bDisplayDeviceModel = true;
+	RightMotionController->MotionSource = FName("Right");
+}
+
+// Called when the game starts or when spawned
+void AVRLocomotionCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Force Reset player rotation incase they spawned facing the wrong way
+	SetActorRotation(FRotator(0, 0, 0));
+
+	// Position Motion Controllers
+	FQuat deviceRotation;
+	GEngine->XRSystem->GetCurrentPose(IXRTrackingSystem::HMDDeviceId, deviceRotation, LastHMDPos);
+	MCOrigin->SetRelativeLocation(FVector(LastHMDPos.X, LastHMDPos.Y, LastHMDPos.Z) * -1);
+	GEngine->XRSystem->SetTrackingOrigin(EHMDTrackingOrigin::Floor);
+}
+
+void AVRLocomotionCharacter::doHMDPosition()
+{
+	// Get New Location
+	FVector devicePosition;
+	FQuat deviceRotation;
+	FVector NewLocation;
+	GEngine->XRSystem->GetCurrentPose(IXRTrackingSystem::HMDDeviceId, deviceRotation, devicePosition);
+
+	// Adjust the Player to the new location
+	FVector HMDDelta = devicePosition - LastHMDPos;
+	NewLocation = HMDDelta + GetActorLocation();
+	SetActorLocation(FVector(NewLocation.X, NewLocation.Y, GetActorLocation().Z));
+
+	// Store new Location
+	LastHMDPos = devicePosition;
+
+	// Adjust the location of the Motion Controllers
+	NewLocation = MCOrigin->GetRelativeLocation() - HMDDelta;
+	MCOrigin->SetRelativeLocation(FVector(NewLocation.X, NewLocation.Y, (NewLocation.Z / -2) - (1.5 * GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight())));
+
+	// Reset the location of the Camera;
+	HMDOrigin->SetRelativeLocation(FVector(0.f, 0.f, devicePosition.Z - GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight()));
+
+	// Adjust the capsule halfheight to be the height of the player;
+	GetCapsuleComponent()->SetCapsuleHalfHeight((devicePosition.Z / 2.f) + 15.f);
+}
+
+// Called every frame
+void AVRLocomotionCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	doHMDPosition();
+}
+
+// Called to bind functionality to input
+void AVRLocomotionCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Plugins/DatVRLocomotion/Source/DatVRLocomotion/Public/VRLocomotionCharacter.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "VRCameraComponent.h"
+#include "MotionControllerComponent.h"
+#include "XRMotionControllerBase.h"
+#include "VRLocomotionCharacter.generated.h"
+
+UCLASS(BlueprintType)
+class DATVRLOCOMOTION_API AVRLocomotionCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRLocomotionCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void doHMDPosition();
+	void setupComponents();
+
+public:
+	// Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		USceneComponent* HMDOrigin;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		UVRCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		USceneComponent* MCOrigin;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		UMotionControllerComponent* LeftMotionController;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		UMotionControllerComponent* RightMotionController;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Variables
+	FVector LastHMDPos;
+};
